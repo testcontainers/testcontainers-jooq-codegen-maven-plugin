38ed5e973ebbf968ad93958892a5dc77eaff9d08
Roman Oborin <oborinroma@yandex.ru>
2023-05-17 11:14:24 +0400

    Custom flyway properties

--- src/main/java/org/testcontainers/jooq/codegen/FlywayMigrationRunner.java
+++ src/main/java/org/testcontainers/jooq/codegen/FlywayMigrationRunner.java
@@ -1,37 +1,20 @@
 package org.testcontainers.jooq.codegen;
 
-import java.util.Properties;
+import java.util.Map;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.configuration.FluentConfiguration;
 
-/**
- * FlywayMigrationRunner executes the flyway migration scripts based on the given configuration properties.
- */
 public class FlywayMigrationRunner {
-    private final FlywayProps flywayProps;
+    private final Map<String, String> flywayProps;
 
-    public FlywayMigrationRunner(FlywayProps flywayProps) {
+    public FlywayMigrationRunner(Map<String, String> flywayProps) {
         this.flywayProps = flywayProps;
     }
 
     void run() {
         FluentConfiguration configuration = new FluentConfiguration();
-        Properties properties = this.getFlywayConfig();
-        configuration.configuration(properties);
+        configuration.configuration(flywayProps);
         Flyway flyway = configuration.load();
         flyway.migrate();
-    }
-
-    private Properties getFlywayConfig() {
-        Properties properties = new Properties();
-        properties.put("flyway.url", flywayProps.getJdbcUrl());
-        properties.put("flyway.user", flywayProps.getUsername());
-        properties.put("flyway.password", flywayProps.getPassword());
-        properties.put("flyway.locations", flywayProps.getLocations());
-        properties.put("flyway.createSchemas", flywayProps.isCreateSchemas());
-        properties.put("flyway.defaultSchema", flywayProps.getDefaultSchema());
-        properties.put("flyway.table", flywayProps.getDefaultSchema());
-
-        return properties;
     }
 }
